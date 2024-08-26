1099bca0df66a1f9ca3ad4202d537f172a78177f
Minhaj <minhajulislammuhammad@gmail.com>
2024-08-26 20:57:11 +0600

    added

---
+++ Codeforces/968/Turtle_and_a_MEX_Problem_(Easy_Version).cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vii;
+typedef vector<ll> vll;
+typedef long double ld;
+
+#define fi first
+#define se second
+#define pb push_back
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(ld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+void solve()
+{
+    ll n, m; cin >> n >> m;
+    ll mx = -1;
+    for(int i = 0; i < n; i++)
+    {
+    	int k; cin >> k;
+    	vii v(k+5, 0);
+    	for(int j = 0; j < k; j++) 
+    	{
+    		int x; cin >> x;
+    		if(x < k+5) v[x] = 1;
+    	}
+    	ll j = 0;
+    	while(v[j]) j++;
+    	j++;
+    	while(v[j]) j++;
+    	mx = max(mx, j);
+    }
+    debug(mx);
+    ll ans;
+    if(m <= mx) ans = 1ll*(m+1)*mx;
+    else ans = 1ll*mx*mx+1ll*(mx+m)*(m-mx+1)/2; 
+    cout << ans << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+    #endif
+
+    int t; cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
