1fd3b4cd7f189e065a7f8fd1aacf1461a4e515c3
Minhaj <minhajulislammuhammad@gmail.com>
2024-08-31 01:49:24 +0600

    added

---
+++ Codeforces/969/Dora_and_Cpp.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vii;
+typedef vector<ll> vll;
+typedef long double ld;
+
+#define fi first
+#define se second
+#define pb push_back
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(ld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+void solve()
+{
+    ll n, a, b; cin >> n >> a >> b;
+    ll ar[n]; 
+    for(int i = 0; i < n; i++) cin >> ar[i];
+
+    if(n == 1)
+    {
+        cout << 0 << "\n"; return;
+    }
+
+    // Bezout theorum
+    // xa + yb = gcd(a, b)*k;
+
+    ll gcd = __gcd(a, b);
+    for(int i = 0; i < n; i++) ar[i] %= gcd;
+    sort(ar, ar+n);
+    ll mn = min(ar[n-1]-ar[0], gcd+ar[n-1]-ar[0]);
+    for(int i = 1; i < n; i++) mn = min(mn, gcd+ar[i-1]-ar[i]);
+    cout << mn << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+    #endif
+
+    int t; cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
