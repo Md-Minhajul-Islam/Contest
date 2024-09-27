0acad68c3642785efe25ed0acc813f3ee97d8378
Minhaj <minhajulislammuhammad@gmail.com>
2024-09-28 01:34:15 +0600

    added

---
+++ Codeforces/975/All_Pairs_Segments.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vii;
+typedef vector<ll> vll;
+typedef long double ld;
+
+#define fi first
+#define se second
+#define pb push_back
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(ld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+void solve()
+{
+    ll n, q; cin >> n >> q;
+    vll a(n);
+    for(auto &x: a) cin >> x;
+    map<ll, ll> m;
+    for(int i = 0; i < n; i++)
+    {
+        ll x = 1ll*i*(n-i)+(n-1-i);
+        ll y = 1ll*(n-1-i)*(i+1);
+        m[x]++;
+        if(i < n-1) m[y] += 1ll*a[i+1]-a[i]-1;
+    }
+    while(q--)
+    {
+        ll k; cin >> k;
+        cout << m[k] <<" ";
+    }
+    cout << "\n";
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+    #endif
+
+    int t; cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
