ef5cc24fff6a1374caae3396cd19ca348dcf79f0
Minhaj <minhajulislammuhammad@gmail.com>
2024-11-01 08:56:35 +0600

    added

---
+++ Codeforces/Edu171/Action_Figures.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vii;
+typedef vector<ll> vll;
+typedef long double ld;
+
+#define fi first
+#define se second
+#define pb push_back
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(ld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+int test_case;
+
+void solve()
+{
+    int n; cin >> n;
+    string s; cin >> s;
+    ll ans = 1ll*n*(n+1)/2ll;
+    ll mx_dis = n/2, have_dis = 0, need_infront = 0;
+    for(int i = n-1; i >= 0; i--)
+    {
+    	if(s[i] == '0')
+    	{
+    		need_infront = max(0ll, need_infront-1);
+    		continue;
+    	}
+    	if(have_dis >= mx_dis) break;
+    	need_infront++;
+    	if(i >= need_infront)
+    	{
+    		ans = ans-i-1ll;
+    		have_dis++;
+    	}
+    	else break;
+    }
+    cout << ans << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+    #endif
+
+    int t_case; cin >> t_case;
+    for(test_case = 1; test_case <= t_case; test_case++)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ Codeforces/Edu171/Black_Cells.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vii;
+typedef vector<ll> vll;
+typedef long double ld;
+
+#define fi first
+#define se second
+#define pb push_back
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(ld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+int test_case;
+
+void solve()
+{
+    int n; cin >> n;
+    vll a(n);
+    for(auto &x: a) cin >> x;
+    if(n == 1)
+    {
+        cout << "1\n"; 
+        return;
+    }
+    if(n%2 == 0)
+    {
+        ll mx = 0;
+
+        for(int i = 0; i < n-1; )
+        {
+            mx = max(mx, a[i+1]-a[i]);
+            i += 2;
+        }
+        cout << mx << "\n";
+        return;
+    }
+    ll ans = 1e18;
+    for(int i = 0; i < n; i+=2)
+    {
+        ll mx = 0;
+        for(int j = i-1; j > 0; j-=2 )
+        {
+            mx = max(mx, a[j]-a[j-1]);
+        }
+        for(int j = i+1; j < n-1; j+=2)
+        {
+            mx = max(mx, a[j+1]-a[j]);
+        }
+        ans = min(mx, ans);
+    }
+    cout << ans << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+    #endif
+
+    int t_case; cin >> t_case;
+    for(test_case = 1; test_case <= t_case; test_case++)
+    {
+        solve();
+    }
+    return 0;
+}
